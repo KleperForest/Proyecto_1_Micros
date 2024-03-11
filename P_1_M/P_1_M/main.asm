f45e7c971f14c19e0c321e085bc3573a4f0887f5
KleperForest <samuelito7gomez@gmail.com>
2024-03-11 02:08:44 -0600

    Create main.asm
    Subir a main

---
+++ P_1_M/P_1_M/main.asm
@@ -0,0 +1,16 @@
+//*********************************************************************
+// Universidad del Valle de Guatemala
+// IE2023: Programación de Microcontroladores
+// Autor: Alan Gomez
+// Proyecto: P_1_M.asm
+// Descripción: Proyecto 1 de Programación de Microcontroladores. 
+//				Versión 11.0.0 
+// Hardware: ATmega328p
+// Created: 3/11/2024 1:50:40 AM
+//********************************************************************* 
+
+
+//--------------------------------------------------------------------
+// Configuración
+//--------------------------------------------------------------------
+
